dbfbd088a22cd085457d25035943f16907b67d13
katrina.savova <katrina@gamingrealms.com>
2022-03-16 21:58:33 +0000

    process issue

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,11 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set , child, get, remove, update, onValue, query, push, onChildChanged} from "firebase/database";
 
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require("webpack");
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if(process.env.NODE_ENV === 'development') {
+    console.log("DEVELOPMENT")
     require('dotenv').config({path: '.env.development'})
 }
 // } else {
